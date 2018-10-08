f6ad4e0dfac564a2a987d55f8fc2a77eb54a2033
absar-intagleo <absar.mushtaq@ispokepakistan.com>
2018-10-08 19:35:24 +0500

    Friendship fix

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -2,7 +2,7 @@ class FriendshipsController < ApplicationController
 	before_action :validate_token, only: :index
 
 	def index
-		@friends = @current_user.friendships.try(:approved)
+		@friends = Friendship.where('user_id = ? OR friend_id = ?', @current_user.id, @current_user.id).try(:approved)
 		render "index.json"
 	end
 end
